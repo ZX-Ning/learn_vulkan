5f95d82fc69ca457fa4f5818bcee825e2579a510
Z.X. Ning <code@zxning.com>
2025-12-25 00:02:19 +0800

    init

---
+++ xmake.lua
@@ -0,0 +1,11 @@
+add_rules("mode.debug", "mode.release")
+
+add_requires("volk", "glfw", "glm", "vulkan-hpp")
+
+target("learn_vulkan", function()
+    set_kind("binary")
+    set_languages("c17", "c++20")
+    add_files("src/**.cpp")
+    add_packages("volk", "glfw", "glm", "vulkan-hpp")
+    add_defines("VULKAN_HPP_NO_CONSTRUCTORS")
+end)
